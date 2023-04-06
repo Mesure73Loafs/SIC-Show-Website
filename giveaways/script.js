2534bbaa8cc62167959192fe8c4a9a785e69be30
Mesure73Loafs <11079595-Mesure73Loafs@users.noreply.replit.com>
2023-04-06 19:42:35 +0000

    Added giveaways page
     - Added a giveaways page which has an input field to enter the code of the giveaway.
     - Added "Enter Giveaway" navbar item

---
+++ giveaways/script.js
@@ -0,0 +1,19 @@
+// Get the input field
+var input = document.getElementById("input");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("enter").click();
+  }
+});
+
+function enter() {
+  if (input.value.toLowerCase() == "testgiveaway") {
+    window.location.href = `${window.location}/42482954`
+  }
+}
